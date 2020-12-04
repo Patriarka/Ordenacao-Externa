6e34fde79ab15f24c1b55186e75de853ded3b79f
marchiartur <marchiartur@gmail.com>
2020-12-04 16:45:27 -0300

    apagando documentos repetidos

--- ordenacao_externa/Ordenacao.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "ManutencaoArquivo.h"
-
-int compare (const void * a, const void * b);
-void Intercalacao(BUFFER *vetor_buffer_entrada, int qtde_buffers_entrada, int qtde_registros, BUFFER* buffer_saida, const char* arquivo_saida);
--- ordenacao_externa/TAD_bufferEntrada.h
+++
@@ -1,8 +0,0 @@
-#include "big_file.h"
-#include "ManutencaoArquivo.h"
-
-BUFFER *Criar_BufferEntrada(int qtde_registros_por_particao, const char *arquivo);
-ITEM_VENDA* Proximo_BufferEntrada(BUFFER* buffer);
-ITEM_VENDA* Consumir_BufferEntrada(BUFFER* buffer, const char* arquivo);
-int Vazio_BufferEntrada(BUFFER* buffer);
-void Destruir(const char *arquivo_entrada, BUFFER *vetor_buffer_entrada, int qtde_buffers);
